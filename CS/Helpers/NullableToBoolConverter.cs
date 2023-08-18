0cd7b784dd07c80a6ee6b0bff01c76dc7ee2c4fd
AndreySlabov <andrey.slabov@devexpress.com>
2023-08-18 12:53:04 +0400

    Update NullableToBoolConverter.cs

--- CS/Helpers/NullableToBoolConverter.cs
+++ CS/Helpers/NullableToBoolConverter.cs
@@ -5,7 +5,9 @@ using System.Windows.Markup;
 
 namespace MDI_Diagram.Helpers {
     public class NullableToBoolConverter : MarkupExtension, IValueConverter {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => !(value is null);
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return value is not null;
+        }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
             throw new NotImplementedException();
