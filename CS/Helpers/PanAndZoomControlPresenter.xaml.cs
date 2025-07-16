00d359ebfdc4d86cc8c3d7a2390f83ff28932b3d
romenskiy.dmitry <dmitry.romenskiy@devexpress.com>
2025-07-16 14:36:15 +0400

    Update the Pan and Zoom panel to support switching between documents

---
+++ CS/Helpers/PanAndZoomControlPresenter.xaml.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.Xpf.Diagram;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MDI_Diagram.Helpers {
+    /// <summary>
+    /// Interaction logic for PanAndZoomControlPresenter.xaml
+    /// </summary>
+    public partial class PanAndZoomControlPresenter : UserControl {
+
+        public static readonly DependencyProperty DiagramProperty =
+            DependencyProperty.Register("Diagram", typeof(DiagramControl), typeof(PanAndZoomControlPresenter), new PropertyMetadata(null));
+
+        public PanAndZoomControlPresenter() {
+            InitializeComponent();
+        }
+
+        private Grid RootGrid { get { return FindName("PART_RootCustomPresenter") as Grid; } }
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e) {
+            base.OnPropertyChanged(e);
+            if (e.Property == DiagramProperty && RootGrid != null && ControlTemplate != null) {
+                RootGrid.Children.Clear();
+                var panAndZoom = ControlTemplate.LoadContent() as DiagramPanAndZoomControl;
+                if (panAndZoom != null) {
+                    panAndZoom.Diagram = e.NewValue as DiagramControl;
+                    this.RootGrid.Children.Add(panAndZoom);
+                }
+            }
+        }
+
+        public DataTemplate ControlTemplate { get; set; }
+        public DiagramControl Diagram {
+            get { return (DiagramControl)GetValue(DiagramProperty); }
+            set { SetValue(DiagramProperty, value); }
+        }
+    }
+}
